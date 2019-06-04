5a905db8639ccb6e794f83bc64791db199a2e72a
Pavel Strashnov <paul.engine@gmail.com>
2019-06-04 20:25:38 +0200

    Remove CRA generated files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
 import ExchangePage from './containers/exchange-page/exchange-page.component';
-import './App.css';
 
 function App() {
   return (
